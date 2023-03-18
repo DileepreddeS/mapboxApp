51387a0e951a66b0a93a74c56206400f7157f1e2
piratedeen <dileepkumar99.reddy@gmail.com>
2023-03-18 23:25:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import * as Mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'mapboxApp';
+export class AppComponent implements OnInit {
+  mapa!: Mapboxgl.Map;
+
+  ngOnInit() {
+    (Mapboxgl as any).accessToken = environment.mapboxKey;
+
+    this.mapa = new Mapboxgl.Map({
+      container: 'mapa-mapbox', // container ID
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/mapbox/light-v11', // style URL
+      center: [79.3547598, 13.6278095], // starting position
+      zoom: 9, // starting zoom
+
+      pitch: 45,
+      bearing: -17.6,
+      antialias: true,
+    });
+    this.createMarker(79.3547598, 13.6278095);
+  }
+
+  createMarker(lng: number, lat: number) {
+    const marker = new Mapboxgl.Marker({
+      draggable: true,
+    })
+      .setLngLat([lng, lat])
+      .addTo(this.mapa);
+    marker.on('drag', () => {
+      console.log(marker.getLngLat());
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  mapboxKey:
+    'pk.eyJ1IjoiaG9yaWsxMSIsImEiOiJjbGZlNDA3MGIwaXYwNDZscjRvaDc3Mzk0In0.Um6SCwuirmCIu-bmqQGUJA',
+};
